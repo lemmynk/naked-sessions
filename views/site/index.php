6f249409c6270c0e3daa4967a3a5406d482f392e
vlada pavlov <pavlowld@gmail.com>
2014-11-08 20:28:08 +0100

    testcode

--- views/site/index.php
+++ views/site/index.php
@@ -1,7 +1,11 @@
 <?php
-/* @var $this yii\web\View */
-$this->title = 'Naked Sessions';
+
+    /* @var $this yii\web\View */
+    $this->title = 'Naked Sessions';
+
 ?>
+
+
 <div class="pages">
     <div class="pages-fade">
         <img src="stories/_thumbs/home-pic.png">
